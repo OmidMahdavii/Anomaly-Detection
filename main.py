e278125ce9fde70196d75a224314ad53b61b538c
Omid Mahdavi <s.omidmahdavi@yahoo.com>
2023-05-19 16:30:49 +0200

    Added load_data

--- main.py
+++ main.py
@@ -5,35 +5,34 @@ from load_data import build_splits
 from experiments.autoencoder import AutoencoderExperiment
 from experiments.gan import GANExperiment
 
-WINDOW_SIZE = 10
+WINDOW_SIZE = 50
+LATENT_SIZE = 8
 THRESHOLD = 0.1
 REG_WEIGHT = 1.0
 
 
 def main(opt):
-    experiment = AutoencoderExperiment() if opt['experiment'] == 'autoencoder' else GANExperiment()
-
     opt['window_size'] = WINDOW_SIZE
+    opt['latent_size'] = LATENT_SIZE
     opt['threshold'] = THRESHOLD
     opt['reg_weight'] = REG_WEIGHT
     
+    experiment = AutoencoderExperiment(opt) if opt['experiment'] == 'autoencoder' else GANExperiment(opt)
+    
     train_loader, validation_loader, test_loader = build_splits(opt)
     
-
     if opt['test']: # Test the model if '--test' flag is set
         experiment.load_checkpoint(f'{opt["output_path"]}/last_checkpoint.pth')
-        test_accuracy, _ = experiment.validate(test_loader)
-        logging.info(f'[TEST] Accuracy: {(100 * test_accuracy):.2f}')
+        recall = experiment.test(test_loader, opt['threshold'])
+        logging.info(f'[TEST] Recall: {(100 * recall):.2f}')
     
     else: # Skip training if '--test' flag is set
         iteration = 0
-
         # Restore last checkpoint
         if os.path.exists(f'{opt["output_path"]}/last_checkpoint.pth'):
-            iteration, train_loss = experiment.load_checkpoint(f'{opt["output_path"]}/last_checkpoint.pth')
+            iteration = experiment.load_checkpoint(f'{opt["output_path"]}/last_checkpoint.pth')
         else:
             logging.info(opt)
-
         # Train loop
         while iteration < opt['max_iterations']:
             train_loss = 0
@@ -43,13 +42,15 @@ def main(opt):
 
             if iteration % opt['print_every'] == 0:
                 logging.info(f'[TRAIN - {iteration}] Loss: {train_loss}')
-                experiment.save_checkpoint(f'{opt["output_path"]}/last_checkpoint.pth', iteration, train_loss)
+                # experiment.save_checkpoint(f'{opt["output_path"]}/last_checkpoint.pth', iteration, train_loss)
 
-            iteration += 1
+            if iteration % opt['validate_every'] == 0:
+                    # Run validation
+                    auc, val_loss = experiment.validate(validation_loader)
+                    logging.info(f'[VAL - {iteration}] Loss: {val_loss} | AUC: {(100 * auc):.2f}')
+                    experiment.save_checkpoint(f'{opt["output_path"]}/last_checkpoint.pth', iteration)
 
-        # Run validation after the training
-        val_accuracy, val_loss = experiment.validate(validation_loader)
-        logging.info(f'[VAL - {iteration}] Loss: {val_loss} | Accuracy: {(100 * val_accuracy):.2f}')        
+            iteration += 1  
 
 
 if __name__ == '__main__':
