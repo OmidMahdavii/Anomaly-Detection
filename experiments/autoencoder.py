670739c3d8f147144f19ace3035d835c2e95236f
Comfortably Numb <masoud@Is-there-anybody-out-there.local>
2023-05-22 17:04:05 +0200

    updated evaluation and validation

--- experiments/autoencoder.py
+++ experiments/autoencoder.py
@@ -1,8 +1,8 @@
+import numpy
 import torch
 from models import Autoencoder
 from sklearn import metrics
-from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix
-
+from sklearn.metrics import recall_score, f1_score, precision_recall_curve
 
 class AutoencoderExperiment: 
     
@@ -55,31 +55,28 @@ class AutoencoderExperiment:
         
         return loss.item()
 
-    def validate(self, loader):
+    def validate(self, loader, threshold=None):
         # notes:
-        # - input parameter to be added: threshold with the default value equal to None. 
-        # - look at these links:
+        # - done -input parameter to be added: threshold with the default value equal to None.
+        # - done - look at these links:
         #   https://scikit-learn.org/stable/modules/generated/sklearn.metrics.PrecisionRecallDisplay.html#sklearn.metrics.PrecisionRecallDisplay.from_predictions
         #   https://scikit-learn.org/stable/modules/generated/sklearn.metrics.average_precision_score.html#sklearn.metrics.average_precision_score
         #   https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_recall_curve.html#sklearn.metrics.precision_recall_curve
-        # - return variables: if threshold==None -> (AP, optimal threshold)
+        # - done - return variables: if threshold==None -> (AP, optimal threshold)
         #                     else -> F1 score
         # - plot the precision recall curve (commented)
         # - use self.test_loss as loss function
         # - I don't think it makes any change but for the loss function the first parameter should be the output and the second
         #   parameter should be the expected output (opposite to what you did)
         # - label anomaly as 1 and normal data as 0 (opposite to what you did)
-        # - move self.model.train() to the end (before return)
+        # - done - move self.model.train() to the end (before return)
 
         self.model.eval()
 
-        accuracy = 0
-
-        predicted = list()
+        if threshold is not None:
+            predicted = list()
         target = list()
         loss_scores = list()
-
-        dict_lebel_score = dict()
 
         with torch.no_grad():
             for x, y in loader:
@@ -89,46 +86,55 @@ class AutoencoderExperiment:
 
                 logits = self.model(x)
 
-                loss = torch.mean(self.criterion(x, logits), dim=2)
+                loss = torch.mean(self.test_loss(logits, x), dim=2)
 
-                predicted_y = loss > self.opt['threshold'].type(torch.int32).ravel()
+                if threshold is not None:
+                    predicted_y = (loss > threshold).type(torch.int32).ravel()
 
                 loss_scores.append(loss.ravel())
-                predicted.append(predicted_y)
-                target.append(y)
+                target.append(y.ravel())
+                if threshold is not None:
+                    predicted.append(predicted_y)
 
-                # accuracy += (pred == y).sum().item()
+        target_labels = torch.cat(target, dim=0)
+        loss_scores = torch.cat(loss_scores, dim=0)
+        if threshold is not None:
+            predicted = torch.cat(predicted, dim=0)
+
+        # dict_lebel_score[0] = loss_scores [loss_scores >= self.opt['threshold']]
+        # dict_lebel_score[1] = loss_scores [loss_scores < self.opt['threshold']]
+        #
+        # fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
+        # roc_auc = metrics.auc(fpr, tpr)
+        #
+        # recall = recall_score(target, predicted)
+        # precision = precision_score(target, predicted)
+        # f1 = f1_score(target, predicted)
+
+        precision, recall, thresholds = precision_recall_curve(target_labels, loss_scores.ravel())
+        f1 = 2 * (precision * recall) / (precision + recall)
+        average_precision = numpy.mean(precision)
+        # average_precision = average_precision_score(target_labels, loss_scores.ravel())
+        optimal_threshold = thresholds[numpy.where( f1 == max(f1, key=lambda x:x) )]
+
 
         self.model.train()
+        if threshold is not None:
+            return f1_score(target_labels, predicted)
+        else:
+            return average_precision, optimal_threshold
 
-        target = torch.cat(target, dim=0)
-        predicted = torch.cat(predicted, dim=0)
-        loss_scores = torch.cat(loss_scores, dim=0)
-
-        dict_lebel_score[0] = loss_scores [loss_scores >= self.opt['threshold']]
-        dict_lebel_score[1] = loss_scores [loss_scores < self.opt['threshold']]
-
-        fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
-        roc_auc = metrics.auc(fpr, tpr)
-
-        recall = recall_score(target, predicted)
-        precision = precision_score(target, predicted)
-        f1 = f1_score(target, predicted)
-
-        return accuracy, f1
-
-    def evaluate(self, loader):
+    def evaluate(self, loader, threshold):
         # notes:
-        # - input parameter to be added: threshold
-        # - return variable: recall
-        # - use self.test_loss as loss function
+        # - done - input parameter to be added: threshold
+        # - done -return variable: recall
+        # - done - use self.test_loss as loss function
 
         self.model.eval()
-        accuracy = 0
+
         predicted = list()
         target = list()
         loss_scores = list()
-        dict_lebel_score = dict()
 
         with torch.no_grad():
             for x, y in loader:
@@ -138,56 +144,59 @@ class AutoencoderExperiment:
 
                 logits = self.model(x)
 
-                loss = torch.mean(self.criterion(x, logits), dim=2)
-
-                predicted_y = loss > self.opt['threshold'].type(torch.int32).ravel()
+                loss = torch.mean(self.test_loss(logits, x), dim=2)
+                predicted_y = (loss > threshold ).type(torch.int32).ravel()
 
                 loss_scores.append(loss.ravel())
                 predicted.append(predicted_y)
-                target.append(y)
+                target.append(y.ravel())
 
-                # accuracy += (pred == y).sum().item()
-
-        self.model.train()
-
-        target = torch.cat(target, dim=0)
+        target_labels = torch.cat(target, dim=0)
         predicted = torch.cat(predicted, dim=0)
-        loss_scores = torch.cat(loss_scores, dim=0)
+        # loss_scores = torch.cat(loss_scores, dim=0)
 
-        # Mahdavi ina unchizaii hastan ke khaste budi
-        dict_lebel_score[0] = loss_scores[loss_scores >= self.opt['threshold']]
-        dict_lebel_score[1] = loss_scores[loss_scores < self.opt['threshold']]
+        # dict_lebel_score[0] = loss_scores [loss_scores >= self.opt['threshold']]
+        # dict_lebel_score[1] = loss_scores [loss_scores < self.opt['threshold']]
+        #
+        # fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
+        # roc_auc = metrics.auc(fpr, tpr)
+        #
+        # recall = recall_score(target, predicted)
+        # precision = precision_score(target, predicted)
+        # f1 = f1_score(target, predicted)
 
-        fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
-        roc_auc = metrics.auc(fpr, tpr)
+        #  New
+        # precision, recall, thresholds = precision_recall_curve(target_labels, loss_scores.ravel())
+        # f1 = 2 * (precision * recall) / (precision + recall)
+        # average_precision = numpy.mean(precision)
+        # optimal_threshold = thresholds[numpy.where(f1 == max(f1, key=lambda x: x))]
 
-        cm = confusion_matrix(target, predicted)
-        recall = recall_score(target, predicted)
-        precision = precision_score(target, predicted)
-        f1 = f1_score(target, predicted)
+        # average_precision = average_precision_score(target_labels, loss_scores.ravel())
+        self.model.train()
+        return recall_score(target_labels, predicted)
 
-        return accuracy, f1
 
-    def find_optimal_for_f1(self, scores, groundTruth):
-        list_f1_th = list()
-        thresholds = set(scores)
-        for th in thresholds:
-            predicted_label = (scores >= th).type(torch.int32).ravel()
-            f1 = f1_score(groundTruth, predicted_label)
-            list_f1_th.append((f1, th))
 
-        return max(list_f1_th, key=lambda x: x[0])[1]
-
-    def find_thr(self, fpr, tpr, thr):
-
-        GOAT = (0, 1)
-        distance = list()
-
-        for pair in zip(tpr, fpr):
-            distance.append(math.sqrt((pair[0] - GOAT[0]) ** 2 + (pair[1] - GOAT[1]) ** 2))
-
-        print(distance)
-        print(min(distance, key=lambda x: x))
-        print(distance.index(min(distance, key=lambda x: x)))
-
-        return thr[distance.index(min(distance, key=lambda x: x))]
+    # def find_optimal_for_f1(self, scores, groundTruth):
+    #     list_f1_th = list()
+    #     thresholds = set(scores)
+    #     for th in thresholds:
+    #         predicted_label = (scores >= th).type(torch.int32).ravel()
+    #         f1 = f1_score(groundTruth, predicted_label)
+    #         list_f1_th.append((f1, th))
+    #
+    #     return max(list_f1_th, key=lambda x: x[0])[1]
+    #
+    # def find_thr(self, fpr, tpr, thr):
+    #
+    #     GOAT = (0, 1)
+    #     distance = list()
+    #
+    #     for pair in zip(tpr, fpr):
+    #         distance.append(math.sqrt((pair[0] - GOAT[0]) ** 2 + (pair[1] - GOAT[1]) ** 2))
+    #
+    #     print(distance)
+    #     print(min(distance, key=lambda x: x))
+    #     print(distance.index(min(distance, key=lambda x: x)))
+    #
+    #     return thr[distance.index(min(distance, key=lambda x: x))]
--- experiments/gan.py
+++ experiments/gan.py
@@ -1,7 +1,8 @@
 import torch
 from models import GAN
+import numpy
 from sklearn import metrics
-from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix, RocCurveDisplay
+from sklearn.metrics import recall_score, f1_score, precision_recall_curve
 
 class GANExperiment: 
     
@@ -88,158 +89,129 @@ class GANExperiment:
         
         return loss.item()
 
-    def validate(self, loader): 
+    def validate(self, loader, threshold=None):
+
         self.model.eval()
 
-        accuracy = 0
-        loss = 0
-
-        predicted_ae = list()
+        if threshold is not None:
+            predicted = list()
         target = list()
         loss_scores = list()
-        predicted_discr_with_recinstructed_input = list()
-        predicted_discr_with_real_input = list()
-
-
 
         with torch.no_grad():
             for x, y in loader:
                 # remember using to(self.device) method for both input and output
-
                 x = x.to(self.device)
                 y = y.to(self.device)
 
-                logits,  discriminator_y, discriminator_x= self.model(x)
+                logits = self.model(x)
 
-                loss = torch.mean(self.criterion(x, logits), dim=2)
+                loss = torch.mean(self.test_loss(logits, x), dim=2)
 
-                predicted_y = loss > self.opt['threshold'].type(torch.int32).ravel()
+                if threshold is not None:
+                    predicted_y = (loss > threshold).type(torch.int32).ravel()
 
                 loss_scores.append(loss.ravel())
-                predicted_ae.append(predicted_y)
-                predicted_discr_with_recinstructed_input.append(discriminator_y.ravel())
-                predicted_discr_with_real_input.append(discriminator_x.ravel())
-                target.append(y)
+                target.append(y.ravel())
+                if threshold is not None:
+                    predicted.append(predicted_y)
 
-                
-                # pred = [...]
-                # accuracy += (pred == y).sum().item()
+        target_labels = torch.cat(target, dim=0)
+        loss_scores = torch.cat(loss_scores, dim=0)
+        if threshold is not None:
+            predicted = torch.cat(predicted, dim=0)
+
+        # dict_lebel_score[0] = loss_scores [loss_scores >= self.opt['threshold']]
+        # dict_lebel_score[1] = loss_scores [loss_scores < self.opt['threshold']]
+        #
+        # fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
+        # roc_auc = metrics.auc(fpr, tpr)
+        #
+        # recall = recall_score(target, predicted)
+        # precision = precision_score(target, predicted)
+        # f1 = f1_score(target, predicted)
+
+        precision, recall, thresholds = precision_recall_curve(target_labels, loss_scores.ravel())
+        f1 = 2 * (precision * recall) / (precision + recall)
+        average_precision = numpy.mean(precision)
+        # average_precision = average_precision_score(target_labels, loss_scores.ravel())
+        optimal_threshold = thresholds[numpy.where( f1 == max(f1, key=lambda x:x) )]
+
 
         self.model.train()
-
-        target = torch.cat(target, dim=0)
-        predicted_ae = torch.cat(predicted_ae, dim=0)
-        loss_scores = torch.cat(loss_scores, dim=0)
-        predicted_discr_with_recinstructed_input = torch.cat(predicted_discr_with_recinstructed_input, dim=0)
-        predicted_discr_with_real_input = torch.cat(predicted_discr_with_real_input, dim=0)
-
-        # RocCurveDisplay.from_predictions(target, loss_scores, name="ROC curve", color="darkorange")
-
-        fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
-        roc_auc = metrics.auc(fpr, tpr)
+        if threshold is not None:
+            return f1_score(target_labels, predicted)
+        else:
+            return average_precision, optimal_threshold
 
 
-        confusion_matrix_ae = confusion_matrix(target, predicted_ae)
-        confusion_matrix_discr = confusion_matrix(target, predicted_discr_with_recinstructed_input)
-
-        recall_ae = recall_score(target, predicted_ae)
-        precision_ae = precision_score(target, predicted_ae)
-        f1_ae = f1_score(target, predicted_ae)
-
-        recall_discr_with_constructed_input = recall_score(target, predicted_discr_with_recinstructed_input)
-        precision_discr_with_constructed_input = precision_score(target, predicted_discr_with_recinstructed_input)
-        f1_discr_with_constructed_input = f1_score(target, predicted_discr_with_recinstructed_input)
-
-        recall_discr = recall_score(target, predicted_discr_with_real_input)
-        precision_discr = precision_score(target, predicted_discr_with_real_input)
-        f1_discr = f1_score(target, predicted_discr_with_real_input)
-
-        return accuracy, loss
-
-    def evaluate(self, loader):
+    def evaluate(self, loader, threshold):
         self.model.eval()
 
-        accuracy = 0
-        loss = 0
-
-        predicted_ae = list()
+        predicted = list()
         target = list()
         loss_scores = list()
-        predicted_discr_with_recinstructed_input = list()
-        predicted_discr_with_real_input = list()
 
         with torch.no_grad():
             for x, y in loader:
                 # remember using to(self.device) method for both input and output
-
                 x = x.to(self.device)
                 y = y.to(self.device)
 
-                logits, discriminator_y, discriminator_x = self.model(x)
+                logits = self.model(x)
 
-                loss = torch.mean(self.criterion(x, logits), dim=2)
-
-                predicted_y = loss > self.opt['threshold'].type(torch.int32).ravel()
+                loss = torch.mean(self.test_loss(logits, x), dim=2)
+                predicted_y = (loss > threshold).type(torch.int32).ravel()
 
                 loss_scores.append(loss.ravel())
-                predicted_ae.append(predicted_y)
-                predicted_discr_with_recinstructed_input.append(discriminator_y.ravel())
-                predicted_discr_with_real_input.append(discriminator_x.ravel())
-                target.append(y)
+                predicted.append(predicted_y)
+                target.append(y.ravel())
 
-                # pred = [...]
-                # accuracy += (pred == y).sum().item()
+        target_labels = torch.cat(target, dim=0)
+        predicted = torch.cat(predicted, dim=0)
+        # loss_scores = torch.cat(loss_scores, dim=0)
 
+        # dict_lebel_score[0] = loss_scores [loss_scores >= self.opt['threshold']]
+        # dict_lebel_score[1] = loss_scores [loss_scores < self.opt['threshold']]
+        #
+        # fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
+        # roc_auc = metrics.auc(fpr, tpr)
+        #
+        # recall = recall_score(target, predicted)
+        # precision = precision_score(target, predicted)
+        # f1 = f1_score(target, predicted)
+
+        #  New
+        # precision, recall, thresholds = precision_recall_curve(target_labels, loss_scores.ravel())
+        # f1 = 2 * (precision * recall) / (precision + recall)
+        # average_precision = numpy.mean(precision)
+        # optimal_threshold = thresholds[numpy.where(f1 == max(f1, key=lambda x: x))]
+
+        # average_precision = average_precision_score(target_labels, loss_scores.ravel())
         self.model.train()
+        return recall_score(target_labels, predicted)
 
-        target = torch.cat(target, dim=0)
-        predicted_ae = torch.cat(predicted_ae, dim=0)
-        loss_scores = torch.cat(loss_scores, dim=0)
-        predicted_discr_with_recinstructed_input = torch.cat(predicted_discr_with_recinstructed_input, dim=0)
-        predicted_discr_with_real_input = torch.cat(predicted_discr_with_real_input, dim=0)
 
-        # RocCurveDisplay.from_predictions(target, loss_scores, name="ROC curve", color="darkorange")
-
-        fpr, tpr, thresholds = metrics.roc_curve(target, loss_scores.ravel(), pos_label=1)
-        roc_auc = metrics.auc(fpr, tpr)
-
-        confusion_matrix_ae = confusion_matrix(target, predicted_ae)
-        confusion_matrix_discr = confusion_matrix(target, predicted_discr_with_recinstructed_input)
-
-        recall_ae = recall_score(target, predicted_ae)
-        precision_ae = precision_score(target, predicted_ae)
-        f1_ae = f1_score(target, predicted_ae)
-
-        recall_discr_with_constructed_input = recall_score(target, predicted_discr_with_recinstructed_input)
-        precision_discr_with_constructed_input = precision_score(target, predicted_discr_with_recinstructed_input)
-        f1_discr_with_constructed_input = f1_score(target, predicted_discr_with_recinstructed_input)
-
-        recall_discr = recall_score(target, predicted_discr_with_real_input)
-        precision_discr = precision_score(target, predicted_discr_with_real_input)
-        f1_discr = f1_score(target, predicted_discr_with_real_input)
-
-        return accuracy, loss
-
-    def find_optimal_for_f1(self, scores, groundTruth):
-        list_f1_th = list()
-        thresholds = set(scores)
-        for th in thresholds:
-            predicted_label = (scores >= th).type(torch.int32).ravel()
-            f1 = f1_score(groundTruth, predicted_label)
-            list_f1_th.append((f1, th))
-
-        return max(list_f1_th, key=lambda x: x[0])[1]
-
-    def find_thr(self, fpr, tpr, thr):
-
-        GOAT = (0, 1)
-        distance = list()
-
-        for pair in zip(tpr, fpr):
-            distance.append(math.sqrt((pair[0] - GOAT[0]) ** 2 + (pair[1] - GOAT[1]) ** 2))
-
-        print(distance)
-        print(min(distance, key=lambda x: x))
-        print(distance.index(min(distance, key=lambda x: x)))
-
-        return thr[distance.index(min(distance, key=lambda x: x))]
+# def find_optimal_for_f1(self, scores, groundTruth):
+#         list_f1_th = list()
+#         thresholds = set(scores)
+#         for th in thresholds:
+#             predicted_label = (scores >= th).type(torch.int32).ravel()
+#             f1 = f1_score(groundTruth, predicted_label)
+#             list_f1_th.append((f1, th))
+#
+#         return max(list_f1_th, key=lambda x: x[0])[1]
+#
+#     def find_thr(self, fpr, tpr, thr):
+#
+#         GOAT = (0, 1)
+#         distance = list()
+#
+#         for pair in zip(tpr, fpr):
+#             distance.append(math.sqrt((pair[0] - GOAT[0]) ** 2 + (pair[1] - GOAT[1]) ** 2))
+#
+#         print(distance)
+#         print(min(distance, key=lambda x: x))
+#         print(distance.index(min(distance, key=lambda x: x)))
+#
+#         return thr[distance.index(min(distance, key=lambda x: x))]
